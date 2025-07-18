9d468dfaede5b3ecf160973abee93409e16df20f
Maksalina95 <maksalinamagazin@gmail.com>
2025-07-19 00:54:47 +0300

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,2 @@
 export const sheetId = '1gBcuPzWv_nH2i7sWyCaERVCjO-hLg8EcndPkEMlNqgw';
-export const baseUrl = `https://opensheet.elk.sh/${sheetId}/Лист1`;
+export const baseUrl = `https://opensheet.elk.sh/${sheetId}/Sheet1`;
