a6f13c7bdd687341e638c88bdcb6b49d2b1584ec
Maksalina95 <maksalinamagazin@gmail.com>
2025-07-19 16:25:45 +0300

    Create home.js
    O

---
+++ scripts.js/home.js
@@ -0,0 +1,21 @@
+// scripts/home.js
+import { fetchSheetData } from "./config.js";
+
+export async function showHome(container) {
+  container.innerHTML = "<h2>Товары</h2><div id='products'></div>";
+  const data = await fetchSheetData();
+  const list = document.getElementById("products");
+
+  data.forEach(item => {
+    if (!item["изображение"]) return;
+    const block = document.createElement("div");
+    block.className = "product";
+    block.innerHTML = `
+      <img src="${item["изображение"]}" alt="${item["название"]}" />
+      <h3>${item["название"]}</h3>
+      <p>${item["описание"]}</p>
+      <strong>${item["цена"]} ₽</strong>
+    `;
+    list.appendChild(block);
+  });
+}
