86c4e1ecf9a6e628fbafd5089ae00720edeb4491
Maksalina95 <maksalinamagazin@gmail.com>
2025-07-19 16:26:04 +0300

    Create config.js
    O

---
+++ scripts.js/config.js
@@ -0,0 +1,22 @@
+// scripts/config.js
+
+const SHEET_ID = "1D0VRNDIEgh1WFPNHNDiF5-Oncdw0Q7Zb3KHbk1LO_08";
+const SHEET_NAME = "Sheet1";
+const URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?sheet=${SHEET_NAME}&tqx=out:json`;
+
+async function fetchSheetData() {
+  const res = await fetch(URL);
+  const text = await res.text();
+  const json = JSON.parse(text.substr(47).slice(0, -2));
+
+  const cols = json.table.cols.map(col => col.label.toLowerCase());
+  return json.table.rows.map(row => {
+    const obj = {};
+    cols.forEach((key, i) => {
+      obj[key] = row.c[i]?.v || "";
+    });
+    return obj;
+  });
+}
+
+export { fetchSheetData };
