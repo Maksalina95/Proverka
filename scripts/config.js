1d25325bcc860d9584d206b7d689cac38ed7a54f
Maksalina95 <maksalinamagazin@gmail.com>
2025-07-19 11:42:02 +0300

    Update config.js
    О

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,2 @@
-// ID твоей Google Таблицы и название листа
-export const sheetId = '1D0VRNDIEgh1WFPNHNDiF5-Oncdw0Q7Zb3KHbk1LO_08';
-export const sheetName = 'Sheet1'; // убедись, что ты переименовала лист
-
-// Ссылка на JSON-данные с OpenSheet
-export const baseUrl = `https://opensheet.elk.sh/${sheetId}/${sheetName}`;
+export const sheetId = '1gBcuPzWv_nH2i7sWyCaERVCjO-hLg8EcndPkEMlNqgw';
+export const baseUrl = `https://opensheet.elk.sh/${sheetId}/Sheet1`;
