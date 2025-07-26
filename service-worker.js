0885c90f2c96fa62a2390adfa9b45688cf43f9b4
Maksalina95 <maksalinamagazin@gmail.com>
2025-07-26 08:01:27 +0300

    Update service-worker.js
    О

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,46 @@
+const CACHE_NAME = 'maksmarket-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/productPage.js',
+  '/catalog.js',
+  '/config.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// Установка
 self.addEventListener("install", e => {
-  console.log("Service Worker installed");
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
   self.skipWaiting();
 });
 
+// Активация
 self.addEventListener("activate", e => {
-  console.log("Service Worker activated");
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
+// Обработка fetch-запросов
 self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
